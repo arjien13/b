9f024de8712466846acffcf16cfee7661f8bb7ed
arjien13 <arjienunag_13@yahoo.com>
2025-04-02 11:12:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function goHome() {
+  window.location.href = "index.html";
+}
+document.querySelector('.bio-photo').style.display = 'none';
+document.querySelector('.profession').style.display = 'none';
+
+
+function showBio() {
+  var elementsToHide = document.querySelectorAll('.intro, .footer, .icons');
+  var elementsToShow = document.querySelectorAll('.bio-section, .bio-paragraph, .bio-photo, .profession');
+  
+  elementsToHide.forEach(element => element.style.display = 'none');
+  elementsToShow.forEach(element => element.style.display = 'block');
+}
