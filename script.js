da0bf1c96983dda1957cb95711b07269c5e6cdc7
arjien13 <arjienunag_13@yahoo.com>
2025-04-03 21:48:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
+// Navigates to the homepage (index.html) when called
 function goHome() {
   window.location.href = "index.html";
 }
 
+// Initializes the page by hiding certain elements
 document.querySelectorAll('.bio-photo, .profession, #resume-section').forEach(element => element.style.display = 'none');
 
-function showBio() {
-  hideElements('.intro, .footer, .icons, #resume-section');
-  showElements('.bio-section, .bio-paragraph, .bio-photo, .profession');
+// SET DISPLAY FUNCTION
+function setDisplay(selector, displayValue) {
+  document.querySelectorAll(selector).forEach(element => element.style.display = displayValue);
 }
 
+// BIO SECTION
+function showBio() {
+  setDisplay('.intro, .footer, .icons, #resume-section', 'none');
+  setDisplay('.bio-section, .bio-paragraph, .bio-photo, .profession', 'block');
+}
+
+// RESUME SECTION
 function showResume() {
-  hideElements('.intro, .footer, .icons, .bio-photo, .profession, .bio-paragraph, .bio-section');
-  showElements('#resume-section');
-  
+  setDisplay('.intro, .footer, .icons, .bio-photo, .profession, .bio-paragraph, .bio-section', 'none');
+  setDisplay('#resume-section', 'block');
   document.querySelector('#resume-section img').addEventListener('click', function(event) {
     window.open(this.src, '_blank');
   });
-}
-
-function hideElements(selector) {
-  document.querySelectorAll(selector).forEach(element => element.style.display = 'none');
-}
-
-function showElements(selector) {
-  document.querySelectorAll(selector).forEach(element => element.style.display = 'block');
 }
