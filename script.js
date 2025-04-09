63c627d525f3fb793c61077706bd4f087e18611e
arjien13 <arjienunag_13@yahoo.com>
2025-04-09 11:56:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function goHome() {
 }
 
 // Initializes the page by hiding certain elements this shows for index viewing
-document.querySelectorAll('.bio-photo, .profession, #resume-section, .Portfolio-Section, .folder-container').forEach(element => element.style.display = 'none');
+document.querySelectorAll('.bio-photo, .profession, #resume-section, .Portfolio-Section, .my-image, .folder-container').forEach(element => element.style.display = 'none');
 
 // SET DISPLAY FUNCTION
 function setDisplay(selector, displayValue) {
@@ -13,13 +13,13 @@ function setDisplay(selector, displayValue) {
 
 // BIO SECTION
 function showBio() {
-  setDisplay('.intro, .footer, .icons, #resume-section, .Portfolio-Section, .folder-container', 'none');
+  setDisplay('.intro, .footer, .icons, #resume-section, .my-image, .Portfolio-Section, .folder-container', 'none');
   setDisplay('.bio-section, .bio-paragraph, .bio-photo, .profession', 'block');
 }
 
 // RESUME SECTION
 function showResume() {
-  setDisplay('.intro, .footer, .icons, .bio-photo, .profession, .bio-paragraph, .bio-section, .Portfolio-Section', 'none');
+  setDisplay('.intro, .footer, .icons, .bio-photo, .profession, .my-image, .bio-paragraph, .bio-section, .Portfolio-Section', 'none');
   setDisplay('#resume-section', 'block');
   document.querySelector('#resume-section img').addEventListener('click', function(event) {
     window.open(this.src, '_blank');
@@ -29,5 +29,5 @@ function showResume() {
 // PORFOLIO SECTION
 function showPortfolio() {
   setDisplay('.intro, .footer, .icons, .bio-photo, .profession, .bio-paragraph, .bio-section, #resume-section', 'none');
-  setDisplay('.Portfolio-Section, .folder-container', 'block');
+  setDisplay('.Portfolio-Section, .my-image, .folder-container', 'block');
 }
